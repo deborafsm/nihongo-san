d31706c805b6f89455bfa828be1814e76ee8b682
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-01-16 22:35:37 -0300

    Update App.js

--- nihongo-san_app/App.js
+++ nihongo-san_app/App.js
@@ -3,9 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 
 function App() {
   return (
-    <View style={{marginTop: 100}}>
-      <Text style={{color: "red", fontSize: 30}}>Nihongo-San App</Text>
+    <View style={{ marginTop: 100 }}>
+      <Text style={{ color: "red", fontSize: 30 }}>Nihongo-San App</Text>
     </View>
   );
 }
+
 export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
