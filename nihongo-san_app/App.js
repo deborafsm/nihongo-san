f7b8cc0c1c95d6614e229be55cb0354b6b2639aa
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-01-24 22:47:16 -0300

    create: component header

--- nihongo-san_app/App.js
+++ nihongo-san_app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import Header from "./src/components/header";
 function App() {
   return (
     <View style={{ marginTop: 100 }}>
       <Text style={styles.container}>Nihongo-San App</Text>
     </View>
+    <Header/>
   );
 }
 
