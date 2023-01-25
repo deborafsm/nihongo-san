994bb41e5f5b5f5ce11aac8fdf71f1792b225f49
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-01-25 15:25:50 -0300

    Update index.js

--- nihongo-san_app/src/components/header/index.js
+++ nihongo-san_app/src/components/header/index.js
@@ -12,7 +12,7 @@ export default function Header() {
     <View style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.username}>Header do App</Text>
-        <TouchableOpacity activeOpacity={0.9}>
+        <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
           <Feather name="use" size={27} color="#fff" />
         </TouchableOpacity>
       </View>
