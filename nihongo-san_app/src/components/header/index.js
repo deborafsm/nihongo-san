dd93a95544c81a61da28f1e1998838092e704991
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-01-26 21:01:48 -0300

    Update Header Style

--- nihongo-san_app/src/components/header/index.js
+++ nihongo-san_app/src/components/header/index.js
@@ -30,7 +30,26 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingTop: statusBarHeight,
     paddingStart: 16,
-    paddingEnd:16,
-    paddingBottom:44
+    paddingEnd: 16,
+    paddingBottom: 44,
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  username: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  buttonUser: {
+    width: 44,
+    height: 44,
+    backgroundColor: "rgba(255,255,255,0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 44 / 2,
   },
 });
