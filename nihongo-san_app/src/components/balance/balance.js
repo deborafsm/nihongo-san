64346a040db7b9f16556c0138548fb8ded9b2779
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-01-30 22:59:02 -0300

    Update balance.js

--- nihongo-san_app/src/components/balance/balance.js
+++ nihongo-san_app/src/components/balance/balance.js
@@ -1,8 +1,14 @@
     import React from 'react';
-    import { View } from 'react-native';
+    import { View, Text, StyleSheet } from 'react-native';
     
     export default function balance() {
      return (
-       <View/>
+      <View style={style.container}>
+        <Text></Text>
+      </View>
       );
     }
+
+const style = StyleSheet.create(({
+    container:{}
+}))
