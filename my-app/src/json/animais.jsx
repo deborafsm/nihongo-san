0a0d2bd3c41cb7228ae352fce1f2cbb2c8abd253
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-02-11 22:05:28 -0300

    Update: json animals

--- my-app/src/json/animais.jsx
+++ my-app/src/json/animais.jsx
@@ -1,10 +1,102 @@
 import cao from "../img/animais/cachorro.png";
+import gato from "../img/animais/gato.png";
+import ovelha from "../img/animais/ovelha.png";
+import caozinho from "../img/animais/puppy.png";
+import bezerro from "../img/animais/bezerro.png";
+import boi from "../img/animais/boi.png";
+import bode from "../img/animais/bode.png";
+import bufalo from "../img/animais/bufalo.png";
+import baleia from "../img/animais/baleia.png";
+import cavalo from "../img/animais/cavalo.png";
+import cervo from "../img/animais/cervo.png";
+import cobra from "../img/animais/cobra.png";
+import coelho from "../img/animais/coelho.png";
+
+
 export const animais = [
   {
-    id: "1",
+    id: "4",
+    pt: "Bezerro",
+    jpn: "Ko-ushi",
+    esc: "子牛",
+    img: bezerro,
+  },
+  {
+    id: "5",
+    pt: "Bode",
+    jpn: "Yagi",
+    esc: "山羊",
+    img: bode,
+  },
+  {
+    id: "6",
+    pt: "Boi",
+    jpn: "Oushi",
+    esc: "牡牛",
+    img: boi,
+  },
+  {
+    id: "7",
+    pt: "Búfalo",
+    jpn: "Yagyuu",
+    esc: "野牛",
+    img: bufalo,
+  },
+  {
+    id: "8",
+    pt: "Baleia",
+    jpn: "Kujira",
+    esc: "鯨",
+    img: baleia,
+  },
+
+  {
+    id: "9",
     pt: "Cão",
     jpn: "Inu",
-    esc: "小犬",
+    esc: "犬",
     img: cao,
+  },
+  {
+    id: "10",
+    pt: "Cãozinho",
+    jpn: "Koinu ",
+    esc: "小犬",
+    img: caozinho,
+  },
+  {
+    id: "11",
+    pt: "Cavalo",
+    jpn: "Uma",
+    esc: "馬",
+    img: cavalo,
+  },
+  {
+    id: "11",
+    pt: "Cervo",
+    jpn: "Shika ",
+    esc: "鹿",
+    img: cervo,
+  },
+  {
+    id: "12",
+    pt: "Cobra",
+    jpn: "Hebi",
+    esc: "蛇",
+    img: cobra,
+  },
+  {
+    id: "13",
+    pt: "Coelho",
+    jpn: "Usagi",
+    esc: "兎",
+    img: coelho,
+  },
+  {
+    id: "1",
+    pt: "Gato",
+    jpn: "Neko",
+    esc: "猫",
+    img: gato,
   },
 ];
