78842826a34fe4ae307159324d6ff9b6f454d60f
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-02-15 22:47:28 -0300

    update: interface roupas

---
+++ my-app/src/json/roupas.jsx
@@ -0,0 +1,11 @@
+import chapeu from "../img/roupas/chapeu.png";
+export const roupas = [
+  {
+    id: "1",
+    pt: "Chapéu",
+    jpn: "bōshi",
+    kana: "ぼうし",
+    kanji: "帽子",
+    img: chapeu,
+  },
+];
--- my-app/src/pages/CardRoupas.jsx
+++ my-app/src/pages/CardRoupas.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Card from "../components/Cards/CardMain";
-import roupas from "../json/roupas.jsx";
+import { roupas } from "../json/roupas.jsx";
 export default function CardRoupas() {
   return (
     <div>
-      <Card></Card>
+      {roupas.map((roupa) => (
+        <Card
+          key={roupa.id}
+          ESC={roupa.jpn}
+          HIRAGANA={roupa.esc}
+          PTB={roupa.pt}
+          img={roupa.img}
+        ></Card>
+      ))}
     </div>
   );
 }
