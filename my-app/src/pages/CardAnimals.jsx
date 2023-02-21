c55131d21c8115dd171799ee7996badc860eaf98
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-02-21 16:17:03 -0300

    Update CardAnimals.jsx

--- my-app/src/pages/CardAnimals.jsx
+++ my-app/src/pages/CardAnimals.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Card from "../components/Cards/CardMain";
 import { ContainerCardSokuon } from "../style/CardPage";
 import { animais } from "../json/animais";
+import Title from '../components/Title/index'
 export default function CardAnimals() {
   return (
     <>
+    <Title text = "Animais"></Title>
       <ContainerCardSokuon>
         {animais.map((animal) => (
           <Card
