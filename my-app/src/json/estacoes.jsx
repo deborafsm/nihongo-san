46a3cb8d9c8b8db662beb467cdd1eae079ba4eea
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-02-23 17:58:39 -0300

    Create estacoes.jsx

---
+++ my-app/src/json/estacoes.jsx
@@ -0,0 +1,40 @@
+
+import primavera from "../img/estacoes_clima/primavera.png";
+import verao from "../img/estacoes_clima/verao.png";
+import outono from "../img/estacoes_clima/outono.png";
+import inverno from "../img/estacoes_clima/inverno.png";
+
+export const estacoes = [
+  {
+    id: "1",
+    pt: "Primavera",
+    jpn: "Haru",
+    esc: "",
+    kanji: "春",
+    img: primavera,
+  },
+  {
+    id: "2",
+    pt: "Verão",
+    jpn: "Natsu ",
+    esc: "",
+    kanji: "夏",
+    img: verao,
+  },
+  {
+    id: "3",
+    pt: "Outono",
+    jpn: "Aki",
+    esc: "",
+    kanji: "秋 ",
+    img: outono,
+  },
+  {
+    id: "4",
+    pt: "Inverno",
+    jpn: "Fuyu",
+    esc: "",
+    kanji: "冬",
+    img: inverno,
+  },
+]
