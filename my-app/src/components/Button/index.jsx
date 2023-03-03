3130e7628632f5b4e0a138335241b48771aefa01
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-03-03 19:19:50 -0300

    create: style file

--- my-app/src/components/Button/index.jsx
+++ my-app/src/components/Button/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
   return (
     <div>
-      
+      <div class="container-button">
+        <a class="button-img" href="">
+          <img src="" alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
