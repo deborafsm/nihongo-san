180d6072ca3abab83e89dd7b16b9fbf88dc6f33d
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-03-06 23:08:20 -0300

    Update CardRoupas.jsx

--- my-app/src/pages/CardRoupas.jsx
+++ my-app/src/pages/CardRoupas.jsx
@@ -3,6 +3,7 @@ import Card from "../components/Cards/CardMain";
 import { roupas } from "../json/roupas.jsx";
 import Title from "../components/Title";
 import Button from "../components/Button";
+import { ContainerCard } from "../style/CardPage";
 export default function CardRoupas() {
   return (
     <div>
@@ -10,15 +11,17 @@ export default function CardRoupas() {
         <Title text="Roupas"></Title>
         <Button></Button>
       </div>
-      {roupas.map((roupa) => (
-        <Card
-          key={roupa.id}
-          ESC={roupa.jpn}
-          HIRAGANA={roupa.kana}
-          PTB={roupa.pt}
-          img={roupa.img}
-        ></Card>
-      ))}
+      <ContainerCard>
+        {roupas.map((roupa) => (
+          <Card
+            key={roupa.id}
+            ESC={roupa.jpn}
+            HIRAGANA={roupa.kana}
+            PTB={roupa.pt}
+            img={roupa.img}
+          ></Card>
+        ))}
+      </ContainerCard>
     </div>
   );
 }
